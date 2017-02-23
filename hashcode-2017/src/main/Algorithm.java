0129e1b4ddddb0806d0c2a21ab530389207158f8
VictorWinberg <dat14vwi@student.lu.se>
2017-02-23 19:46:14 +0100

    Structure fix

--- hashcode-2017/src/main/Algorithm.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public interface Algorithm {
-	
-	public void perform();
-	
-}
--- hashcode-2017/src/main/ScoreCalculator.java
+++
@@ -1,7 +0,0 @@
-package main;
-public class ScoreCalculator {
-	
-	public static int getScore(Algorithm algorithm) {
-		return 0;
-	}
-}
