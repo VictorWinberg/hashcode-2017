4b903d29811c3c6e4455d8be6e480ddf04521b24
Per-Victor Persson <dracyr@gmail.com>
2017-02-23 22:03:57 +0100

    start to maybe

--- ruby_ftw.rb
+++ ruby_ftw.rb
@@ -34,6 +34,7 @@ end
 videos = Hash.new(0)
 
 cache_requests = Hash.new { |hash, key| hash[key] = Array.new(n_videos, 0) }
+all_requests = []
 
 n_requests.times do |i|
   print("\r#{i}/#{n_requests}             ") if i % 100 == 0
@@ -42,19 +43,41 @@ n_requests.times do |i|
   request_endpoint = r[1]
   request_amount = r[2]
 
-  # if connected
-  # for each cache
-  n_caches.times do |j|
-    # print("\r#{i}/#{n_requests} #{j}/#{n_caches}             ")
-    # if endpoint is connected to cache
-    cache_has_endpoint = endpoints[request_endpoint].keys.include?(j)
-    if cache_has_endpoint
-      cache_requests[j][request_video] = 1
-    end
-  end
-
+  # # if connected
+  # # for each cache
+  # n_caches.times do |j|
+  #   # print("\r#{i}/#{n_requests} #{j}/#{n_caches}             ")
+  #   # if endpoint is connected to cache
+  #   cache_has_endpoint = endpoints[request_endpoint].keys.include?(j)
+  #   if cache_has_endpoint
+  #     cache_requests[j][request_video] = 1
+  #   end
+  # end
+  all_requests << [request_video, request_endpoint, request_amount]
   videos[request_video] += request_amount
 end
+
+all_requests.sort_by {|a| a[0]}
+
+def find_sorted_request(requests, video_id)
+  i = 0
+
+  a = requests.bsearch do |x|
+    x >= video_id
+  end
+
+  # while i < requests.length
+  #   return nil if requests[i][0] > video_id
+  #   return requests[i] if requests[i][0] == video_id
+  #   i += 1
+  # end
+  nil
+end
+
+
+find_sorted_request(all_requests, 1)
+
+exit
 
 videos = videos.sort_by {|k,v| v}.reverse
 
@@ -71,11 +94,17 @@ n_caches.times do |i|
 
   retries = 0
   videos_copy = videos.dup
-  while rem_size > 0 && retries < 1000
-    video = videos_copy[rand.rand([videos_copy.size, 10].min)][0]
+  while rem_size > 0 && retries < 500
+    #video = videos_copy[rand.rand([videos_copy.size, 10].min)][0]
+    video = videos_copy[rand.rand([videos_copy.size, 100].min)][0]
     video_size = video_sizes[video]
-    next if video_size > (cache_size / 5)
-    next if cache_requests[i][video] == 0
+    next if video_size > (cache_size / 2)
+
+    request = find_sorted_request(all_requests, video)
+    next if !request
+    request_endpoint = request[1]
+    next if !endpoints[request_endpoint].keys.include?(i)
+    #next if cache_requests[i][video] == 0
 
     if rem_size - video_size >= 0 && !caches[i].include?(video)
       videos_copy.pop
